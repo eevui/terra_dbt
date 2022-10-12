7c77491862841dd59b02ad1f98211eb749ba1437
robel91 <92173230+robel91@users.noreply.github.com>
2022-10-12 16:09:54 -0500

    format the sql code

--- models/silver/silver__staking.sql
+++ models/silver/silver__staking.sql
@@ -1,113 +1,104 @@
-{{ config(
-    materialized = 'incremental',
-    cluster_by = ['_inserted_timestamp::DATE'],
-    unique_key = 'tx_id'
-) }}
+{{
+    config(
+        materialized="incremental",
+        cluster_by=["_inserted_timestamp::DATE"],
+        unique_key="tx_id",
+    )
+}}
 
+with
+    delegated as (
+        select
+            block_id,
+            block_timestamp,
+            tx_id,
+            tx_succeeded,
+            chain_id,
+            message_index as msg_index,
+            'Delegate' as action,
+            attributes:transfer:recipient::string as delegator_address,
+            attributes:delegate:amount / pow(10, 6)::integer as amount,
+            attributes:delegate:validator::string as validator_address,
+            _ingested_at,
+            _inserted_timestamp
 
---Data pulled from messages / delegate
-with delegated AS (
-SELECT 
-    BLOCK_ID,
-    BLOCK_TIMESTAMP,
-    TX_ID,
-    TX_SUCCEEDED,
-    CHAIN_ID,
-    MESSAGE_INDEX As MSG_INDEX,
-    'Delegate' AS Action,
-    ATTRIBUTES:transfer :recipient :: STRING AS delegator_address,
-    ATTRIBUTES:delegate :amount/pow(10,6) :: INTEGER AS amount,
-    ATTRIBUTES:delegate :validator :: STRING AS validator_address
+        from {{ ref("silver__messages") }}
+        where
+            message_type ilike '%msgdelegate%'
+            and attributes:message:module::string = 'staking'
+            and tx_succeeded = 'True'
+            and {{ incremental_load_filter("_inserted_timestamp") }}
     
-FROM 
-    {{ref ('silver__messages')}}
-WHERE 
-    message_type ilike '%msgdelegate%'
-    and ATTRIBUTES:message :module :: STRING = 'staking'
-    and TX_SUCCEEDED = 'True'
-    and {{ incremental_load_filter("_inserted_timestamp") }}
-) 
- select *
- from delegated 
- where tx_id = '197CE8367FCB5059D4B3650C389EABDEDA9B6464C75B92D8689F6C83891EC631'
- 
- --Data pulled from messages / undelegate
-undelegated AS (
-SELECT 
-    BLOCK_ID,
-    BLOCK_TIMESTAMP,
-    TX_ID,
-    TX_SUCCEEDED,
-    CHAIN_ID,
-    MESSAGE_INDEX As MSG_INDEX,
-    'Undelegate' AS Action,
-    ATTRIBUTES:transfer :recipient_0 :: STRING AS delegator_address,
-    ATTRIBUTES:unbond :amount/pow(10,6) :: INTEGER AS amount,
-    ATTRIBUTES:unbond :validator :: STRING AS validator_address
-FROM 
-    {{ref ('silver__messages')}}
-WHERE 
-    message_type ilike '%msgundelegate%'
-    and ATTRIBUTES:message :module :: STRING = 'staking'
-    and TX_SUCCEEDED = 'True'
-    and {{ incremental_load_filter("_inserted_timestamp") }}
- )
- 
- --Dta pulled from message/redelegate action
- ,redelegate AS (
-    SELECT 
-        BLOCK_ID,
-        BLOCK_TIMESTAMP,
-        TX_ID,
-        TX_SUCCEEDED,
-        CHAIN_ID,
-        MESSAGE_INDEX As MSG_INDEX,
-        'Redelegate' AS Action,
-        ATTRIBUTES:transfer :recipient :: STRING AS delegator_address,
-        ATTRIBUTES:redelegate :amount/pow(10,6) :: INTEGER AS amount,
-        ATTRIBUTES:redelegate :destination_validator :: STRING AS validator_address,
-        ATTRIBUTES:redelegate :source_validator :: STRING AS validator_src_address
-    FROM 
-       {{ref ('silver__messages')}}
-    WHERE 
-        message_type ilike '%MsgBeginRedelegate%'
-        and ATTRIBUTES:message :module :: STRING = 'staking'
-        and TX_SUCCEEDED = 'True'
-        and {{ incremental_load_filter("_inserted_timestamp") }}
-     
- )
+    ),
+    undelegated as (
+        select
+            block_id,
+            block_timestamp,
+            tx_id,
+            tx_succeeded,
+            chain_id,
+            message_index as msg_index,
+            'Undelegate' as action,
+            attributes:transfer:recipient_0::string as delegator_address,
+            attributes:unbond:amount / pow(10, 6)::integer as amount,
+            attributes:unbond:validator::string as validator_address,
+            _ingested_at,
+            _inserted_timestamp
+        from {{ ref("silver__messages") }}
+        where
+            message_type ilike '%msgundelegate%'
+            and attributes:message:module::string = 'staking'
+            and tx_succeeded = 'True'
+            and {{ incremental_load_filter("_inserted_timestamp") }}
+    
+    ),
+    redelegated as (
+        select
+            block_id,
+            block_timestamp,
+            tx_id,
+            tx_succeeded,
+            chain_id,
+            message_index as msg_index,
+            'Redelegate' as action,
+            attributes:transfer:recipient::string as delegator_address,
+            attributes:redelegate:amount / pow(10, 6)::integer as amount,
+            attributes:redelegate:destination_validator::string as validator_address,
+            _ingested_at,
+            _inserted_timestamp
 
-  --src_address/redelegate
- ,src_address AS (
-    SELECT
-        TX_ID,
-        ATTRIBUTES:redelegate :source_validator :: STRING AS validator_src_address
-    FROM 
-        terra_dev.silver.messages
-    WHERE 
-        message_type ilike '%MsgBeginRedelegate%'
-        and ATTRIBUTES:message :module :: STRING = 'staking'
-        and TX_SUCCEEDED = 'True'
-        and {{ incremental_load_filter("_inserted_timestamp") }}
-) 
- 
-  --union them all
- ,union_delegations AS (
-    SELECT *
-    FROM 
-        delegate
-    UNION ALL
-    SELECT *
-    FROM
-        undelegate
-    UNION ALL
-    SELECT *
-    FROM 
-        redelegate
- ) 
+        from {{ ref("silver__messages") }}
+        where
+            message_type ilike '%MsgBeginRedelegate%'
+            and attributes:message:module::string = 'staking'
+            and tx_succeeded = 'True'
+            and {{ incremental_load_filter("_inserted_timestamp") }}
 
- SELECT union_delegations.*, src_address.validator_src_address
- FROM 
-    union_delegations 
-    LEFT OUTER JOIN src_address
-    ON union_delegations.tx_id = src_address.tx_id
+   
+    ),
+    src_address as (
+        select
+            tx_id,
+            attributes:redelegate:source_validator::string as validator_src_address
+        from {{ ref("silver__messages") }}
+        where
+            message_type ilike '%MsgBeginRedelegate%'
+            and attributes:message:module::string = 'staking'
+            and tx_succeeded = 'True'
+            and {{ incremental_load_filter("_inserted_timestamp") }}
+    
+    ),
+    union_delegations as (
+        select *
+        from delegated
+        union all
+        select *
+        from undelegated
+        union all
+        select *
+        from redelegated
+    )
+
+select union_delegations.*, src_address.validator_src_address
+from union_delegations
+left outer join src_address on union_delegations.tx_id = src_address.tx_id
