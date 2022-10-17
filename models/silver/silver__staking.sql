475a692405d048e06d4350076c9f3bdec7044976
robel91 <92173230+robel91@users.noreply.github.com>
2022-10-17 12:39:15 -0500

    change the format to keep consistency

--- models/silver/silver__staking.sql
+++ models/silver/silver__staking.sql
@@ -9,7 +9,7 @@
 with
     delegated as (
         select
-            'terra' AS BLOCKCHAIN,
+            'terra' as blockchain,
             block_id,
             block_timestamp,
             tx_id,
@@ -17,9 +17,9 @@ with
             chain_id,
             message_index as msg_index,
             'Delegate' as action,
-            MESSAGE_VALUE :delegator_address :: STRING AS delegator_address,
-            {{change_decimal('MESSAGE_VALUE:amount:amount')}} AS amount,
-            MESSAGE_VALUE :validator_address :: STRING AS validator_address,
+            message_value:delegator_address::string as delegator_address,
+            {{ change_decimal("MESSAGE_VALUE:amount:amount") }} as amount,
+            message_value:validator_address::string as validator_address,
             _ingested_at,
             _inserted_timestamp
 
@@ -29,38 +29,38 @@ with
             and attributes:message:module::string = 'staking'
             and tx_succeeded = 'True'
             and {{ incremental_load_filter("_inserted_timestamp") }}
-    
+
     ),
-    message_exec AS (
-        SELECT 
-            'terra' AS BLOCKCHAIN,
-            BLOCK_ID,
-            BLOCK_TIMESTAMP,
-            TX_ID,
-            TX_SUCCEEDED, 
-            CHAIN_ID,
-            MESSAGE_INDEX As MSG_INDEX,
-            'Delegate' AS Action,
-            message_value.value :delegator_address :: STRING AS delegator_address,
-            message_value.value:amount :amount/pow(10,6) :: INTEGER AS amount,
-            message_value.value :validator_address :: STRING AS validator_address,
-            _INGESTED_AT,
-            _INSERTED_TIMESTAMP
-    
-    
-        FROM 
+    message_exec as (
+        select
+            'terra' as blockchain,
+            block_id,
+            block_timestamp,
+            tx_id,
+            tx_succeeded,
+            chain_id,
+            message_index as msg_index,
+            'Delegate' as action,
+            message_value.value:delegator_address::string as delegator_address,
+            message_value.value:amount:amount / pow(10, 6)::integer as amount,
+            message_value.value:validator_address::string as validator_address,
+            _ingested_at,
+            _inserted_timestamp
+
+
+        from
             {{ ref("silver__messages") }},
-            LATERAL flatten (message_value:msgs) message_value
-        WHERE 
+            lateral flatten(message_value:msgs) message_value
+        where
             tx_id = '40F38644FB9C32F8AF134BAEC61C07AFE92F1A247903D6CF192663F4B52D227C'
-            and message_value.value :"@type" ilike '%MsgDelegate'
-            and message_type ilike'%MsgExec%'
-            and TX_SUCCEEDED = 'True'
+            and message_value.value:"@type" ilike '%MsgDelegate'
+            and message_type ilike '%MsgExec%'
+            and tx_succeeded = 'True'
             and {{ incremental_load_filter("_inserted_timestamp") }}
     ),
     undelegated as (
         select
-            'terra' AS BLOCKCHAIN,
+            'terra' as blockchain,
             block_id,
             block_timestamp,
             tx_id,
@@ -68,9 +68,9 @@ with
             chain_id,
             message_index as msg_index,
             'Undelegate' as action,
-            MESSAGE_VALUE :delegator_address :: STRING AS delegator_address,
-            {{change_decimal('MESSAGE_VALUE:amount:amount')}} AS amount,
-            MESSAGE_VALUE :validator_address :: STRING AS validator_address,
+            message_value:delegator_address::string as delegator_address,
+            {{ change_decimal("MESSAGE_VALUE:amount:amount") }} as amount,
+            message_value:validator_address::string as validator_address,
             _ingested_at,
             _inserted_timestamp
         from {{ ref("silver__messages") }}
@@ -79,11 +79,11 @@ with
             and attributes:message:module::string = 'staking'
             and tx_succeeded = 'True'
             and {{ incremental_load_filter("_inserted_timestamp") }}
-    
+
     ),
     redelegated as (
         select
-            'terra' AS BLOCKCHAIN,
+            'terra' as blockchain,
             block_id,
             block_timestamp,
             tx_id,
@@ -91,10 +91,10 @@ with
             chain_id,
             message_index as msg_index,
             'Redelegate' as action,
-            MESSAGE_VALUE :delegator_address :: STRING AS delegator_address,
-            {{change_decimal('MESSAGE_VALUE:amount:amount')}} AS amount,
-            --MESSAGE_VALUE:amount:amount / pow(10, 6)::integer as amount,
-            MESSAGE_VALUE :validator_dst_address :: STRING AS validator_address,
+            message_value:delegator_address::string as delegator_address,
+            {{ change_decimal("MESSAGE_VALUE:amount:amount") }} as amount,
+            -- MESSAGE_VALUE:amount:amount / pow(10, 6)::integer as amount,
+            message_value:validator_dst_address::string as validator_address,
             _ingested_at,
             _inserted_timestamp
 
@@ -105,24 +105,23 @@ with
             and tx_succeeded = 'True'
             and {{ incremental_load_filter("_inserted_timestamp") }}
 
-   
+
     ),
     src_address as (
         select
-            tx_id,
-            MESSAGE_VALUE :validator_src_address :: STRING AS validator_src_address
+            tx_id, message_value:validator_src_address::string as validator_src_address
         from {{ ref("silver__messages") }}
         where
             message_type ilike '%MsgBeginRedelegate%'
             and attributes:message:module::string = 'staking'
             and tx_succeeded = 'True'
             and {{ incremental_load_filter("_inserted_timestamp") }}
-    
+
     ),
     union_delegations as (
         select *
         from delegated
-        union all 
+        union all
         select *
         from message_exec
         union all
