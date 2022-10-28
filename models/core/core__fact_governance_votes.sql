482df321435adcb9e433542c9979ada44256556c
Eevui <eevui@protonmail.com>
2022-10-28 11:09:33 +0800

    governance_votes: enumerate core view columns

--- models/core/core__fact_governance_votes.sql
+++ models/core/core__fact_governance_votes.sql
@@ -4,7 +4,18 @@
 
 with governance_votes as (
     select
-        *
+        tx_id,
+        block_id,
+        block_timestamp,
+        blockchain,
+        chain_id,
+        voter,
+        proposal_id,
+        vote_option,
+        vote_option_text,
+        vote_weight,
+        tx_succeeded,
+        _inserted_timestamp
     from {{ ref('silver__governance_votes') }}
 )
 
