b3f72fa5bd778ef4844a35a6ec3598d7310a2a37
D0R11 <dorii0010@protonmail.com>
2022-11-02 12:27:15 +0800

    changed json merge type to object

--- macros/json_merge.sql
+++ macros/json_merge.sql
@@ -1,6 +1,6 @@
 {% macro create_json_merge() -%}
-CREATE OR REPLACE FUNCTION {{target.database}}.{{target.schema}}.json_merge(o1 VARIANT, o2 VARIANT)
-  RETURNS VARIANT
+CREATE OR REPLACE FUNCTION {{target.database}}.{{target.schema}}.json_merge(o1 OBJECT, o2 OBJECT)
+  RETURNS OBJECT
   LANGUAGE JAVASCRIPT
 AS
 $$
