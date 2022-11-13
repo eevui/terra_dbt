acf1668364eb64bf58d5cd7ee869384a19587c68
D0R11 <dorii0010@protonmail.com>
2022-11-13 18:00:18 +0800

    changed json merge to python instead of JS

--- macros/json_merge.sql
+++ macros/json_merge.sql
@@ -1,10 +1,15 @@
 {% macro create_json_merge() -%}
-CREATE OR REPLACE FUNCTION {{target.database}}.{{target.schema}}.json_merge(o1 OBJECT, o2 OBJECT)
-  RETURNS OBJECT
-  LANGUAGE JAVASCRIPT
-AS
-$$
-  return Object.assign(O1, O2);
-$$
-;
+CREATE
+OR REPLACE FUNCTION {{ target.database }}.{{ target.schema }}.json_merge(
+  o1 OBJECT,
+  o2 OBJECT
+) returns object
+language python
+runtime_version = 3.8
+handler = 'json_merge'
+as $$
+def json_merge(o1, o2):
+    o1.update(o2)
+    return o1
+$$;
 {%- endmacro %}
